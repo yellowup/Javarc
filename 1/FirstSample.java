d842dd0c5006490ff3571d22f8667affe807882e
yellowup <ssmbs1992@gmail.com>
2019-01-12 06:21:01 +0800

    Test

---
+++ 1/FirstSample.java
@@ -0,0 +1,10 @@
+public class FirstSample{
+    public static void main(String[] args)
+    {
+        String name = "shang";
+        System.out.println("Hellow World,"+name);
+        int n = name.length();
+        System.out.println(n);
+
+    }
+}
---
+++ 1/InputTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class InputTest
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hellow ");
+        String name = in.nextLxine();
+    }
+}
